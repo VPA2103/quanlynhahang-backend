8b5f167ca0a5c820bc0f719774d475e225413a2b
VPA2103 <150929603+VPA2103@users.noreply.github.com>
2025-10-28 14:27:09 +0700

    xong api cua NhanVien

---
+++ routes/nhanvien.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/vpa/quanlynhahang-backend/handlers"
+)
+
+func NhanVienRoutes(r *gin.Engine) {
+	nhanvien := r.Group("/nhanvien")
+	{
+		nhanvien.POST("/", handlers.CreateNhanVien)
+		nhanvien.GET("/", handlers.GetAllNhanVien)
+		nhanvien.GET("/:id", handlers.GetNhanVienByID)
+		nhanvien.PUT("/:id", handlers.UpdateNhanVien)
+		nhanvien.DELETE("/:id", handlers.DeleteNhanVien)
+	}
+}
