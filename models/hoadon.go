49351112746e46a946ab7f97366db35470c63837
VPA2103 <150929603+VPA2103@users.noreply.github.com>
2025-12-08 12:48:20 +0700

    build thành công vào docker và chạy

--- models/hoadon.go
+++ models/hoadon.go
@@ -6,10 +6,10 @@ type HoaDon struct {
 	MaHD      string `gorm:"primaryKey;size:10;autoIncrement"`
 	MaBan     string `gorm:"size:10"`
 	NgayLap   time.Time
-	GioLap    string
+	GioLap    time.Time
 	TongTien  float64
-	TrangThai string
+	TrangThai uint
 
-	MaNVOrder     string   `gorm:"size:10"` // foreign key
+	MaNVOrder     uint     `gorm:"size:10"` // foreign key
 	NhanVienOrder NhanVien `gorm:"foreignKey:MaNVOrder;references:MaNV"`
 }
